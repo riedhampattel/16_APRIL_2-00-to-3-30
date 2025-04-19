fa77bda89f0d0b01bb5dae6acd528fcde874f91f
riedhampattel <riedhampattel.tops@gmail.com>
2025-04-19 16:02:55 +0530

    Add files via upload

---
+++ 16_APRIL_2-00-to-3-30/C Programming/Basic Syntax/1.c
@@ -0,0 +1,18 @@
+// single line comment
+/*
+	multiline comment
+	# ---> pre processor
+	include ---> directive
+	stdio.h ---> standard input output header file
+	int ---> return type
+	main ---> function name
+*/
+#include<stdio.h>//linkimg section
+int main()
+{
+	printf("Hello World!\n");
+	printf("How are you?");
+	printf("\n");
+	printf("\tBye Bye");
+	return 0;
+}
---
+++ 16_APRIL_2-00-to-3-30/C Programming/Basic Syntax/2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int roll=101;
+	long int fees=75000;
+	float percentage = 85.65;
+	char grade='B';
+	
+	printf("Student's roll no = %d",roll);
+	printf("\nStudent's fees = %ld",fees);
+	printf("\nStudent's Percentage = %.2f",percentage);
+	printf("\nStudent's grade = %c",grade);
+	return 0;
+}
