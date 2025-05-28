05f9301ae55d8e797ff4de099d1a379c9ea665fa
riedhampattel <riedhampattel.tops@gmail.com>
2025-05-28 12:57:21 +0530

    updated

--- C++/Loops/6.cpp
+++ C++/Loops/6.cpp
@@ -5,3 +5,18 @@ Input number of terms: 5
 1 + 11 + 111 + 1111 + 11111
 The sum of the series is: 12345
 */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int term,num=1,sum=0,i;
+    cout<<"Enter the terms = ";
+    cin>>term;//3
+    for(i=1;i<=term;i++)
+    {
+        sum = sum + num;
+        num = (num*10) + 1;
+    }
+    cout<<"\nThe addition is = "<<sum;
+    return 0;
+}
--- C++/Loops/7.cpp
+++ C++/Loops/7.cpp
@@ -11,3 +11,42 @@ Write a c++ program to dislay this pattern
    * *
     *
 */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row,i,j,k;
+    cout<<"Enter odd row numbers only = ";
+    cin>>row;
+    int nrow = (row/2) + 1;
+    int spc = nrow-1;
+    for(i=1;i<=nrow;i++)
+    {
+        for(j=1;j<=spc;j++)
+        {
+            cout<<" ";
+        }
+        for(k=1;k<=i;k++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+        spc--;
+    }   
+    int rrow = row - nrow;
+    int star = rrow;
+    for(i=1;i<=rrow;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<" ";
+        }
+        for(k=1;k<=star;k++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+        star--;
+    }
+    return 0;
+}
